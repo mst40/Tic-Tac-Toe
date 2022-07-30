2bc692e2747e53b1d5218fefd62236be47388401
Masato <35ma.ma.ma19@gmail.com>
2022-07-31 00:08:40 +0900

    add status

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import './index.css';
 //Square
 //tag内の{}は描画されるもの
 function Square(props) {
-  render() {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
   
 //Board
@@ -20,15 +18,23 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      //状態を保持
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     //slice()でsquareをコピー
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -41,8 +47,15 @@ class Board extends React.Component {
   }
   
   render() {
-    const status = 'Next player: X';
-  
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -83,25 +96,44 @@ class Game extends React.Component {
   }
 }
   
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
   
-// <div>はビルド時にReact.createElement('div')に変換される
-//このReactコンポーネントは独立して動くので再帰可能
-class ShoppiingList extends React.Component {
-    render() {
-        return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        );
-    }
-}
+// // <div>はビルド時にReact.createElement('div')に変換される
+// //このReactコンポーネントは独立して動くので再帰可能
+// class ShoppiingList extends React.Component {
+//     render() {
+//         return (
+//             <div className="shopping-list">
+//                 <h1>Shopping List for {this.props.name}</h1>
+//                 <ul>
+//                     <li>Instagram</li>
+//                     <li>WhatsApp</li>
+//                     <li>Oculus</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
 
